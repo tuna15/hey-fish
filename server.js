22dad2d4a6e4650022798fe04259b9986b9047c0
tuna15 <sir.rainman@gmail.com>
2017-09-24 17:22:05 -0400

    it's on the internet

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(express.static(__dirname + '/views'));
 
 app.use((req, res, next) => {
-  res.render('/views/index.html');
+  res.render('/index.html');
 });
 
-app.listen(3000, () => {
-  console.log('server is up on port 3000')
+app.listen(port, () => {
+  console.log(`server is up on port ${port}`);
 });
